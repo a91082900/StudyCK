53ee2f197aad192ebf8423dd7ca6e756d2e98bd7
Jerry Wu <a91082900@gmail.com>
2018-10-30 23:37:17 +0800

    Fixed setTitle bug; Changed the color of CustomTabs

--- app/src/main/java/tech/goda/studyck/MainActivity.java
+++ app/src/main/java/tech/goda/studyck/MainActivity.java
@@ -1,44 +1,33 @@
 package tech.goda.studyck;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
-import android.support.annotation.NonNull;
 import android.support.customtabs.CustomTabsIntent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.provider.DocumentFile;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -74,12 +63,14 @@ public class MainActivity extends AppCompatActivity
     private TextView navName;
     private Fragment mCurrentFragment = null;
     private ArrayList<Fragment> fragmentArrayList = null;
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        title = getString(R.string.app_name);
         Log.e("Directory", uploadFilePath);
         /*uploadButton = findViewById(R.id.uploadButton);
         choose = findViewById(R.id.choose);
@@ -387,11 +378,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
         //item.setChecked(true);
         Toast.makeText(getApplicationContext(), "你點選了"+item.toString(), Toast.LENGTH_SHORT).show();
-
+        title = item.toString();
         changeFragment(id);
-        setTitle(item.getTitle());
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -412,6 +403,7 @@ public class MainActivity extends AppCompatActivity
             /*Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://210.71.78.163/clash/"));
             startActivity(browserIntent);*/
             CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
             CustomTabsIntent customTabsIntent = builder.build();
             customTabsIntent.launchUrl(this, Uri.parse("http://210.71.78.163/clash/"));
         } else if (id == R.id.nav_info) {
@@ -424,6 +416,7 @@ public class MainActivity extends AppCompatActivity
 
         Fragment newFragment;
         if (fragment != null) {
+            setTitle(title);
             newFragment = getSupportFragmentManager().findFragmentByTag(fragment.getClass().getName());
 
 
